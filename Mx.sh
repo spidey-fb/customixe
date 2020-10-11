9a7151f7aefd80a85e24aab484e60a4ae99cc68d
spidey-fb <71762204+spidey-fb@users.noreply.github.com>
2020-10-11 16:53:50 +0530

    Update Mx.sh

--- Mx.sh
+++ Mx.sh
@@ -2,14 +2,60 @@
 
 read -p 'do u want to remove motd y/n: ' userinput1;
 #
+
+
+
+
+echo -e "\e[36mauthor : spidey-fb \e[0m";
+
+read -p 'do u want to remove motd y/n' userinput1;
+
+#
+
+if [ "${userinput1:-}" = "y" ]
+
+then
+
+	cd
+
+fi
+
+if [ "${userinput1:-}" = "y" ]
+
+then
+
+	cd  ../usr/etc
+
+fi
+
 if [ "${userinput1:-}" = "y " ]
+
 then
-	rm motd
+
+	touch  motd
+
 fi
-if [ "${userinput1:-}" != "y" ]
+
+if [ "${userinput1:-}" = "y " ]
+
 then
-	echo -e "\e[35mstopped\e[0m";
+
+	vim  motd
+
 fi
+
+if [ "${userinput1:-}" = "n" ]
+
+then
+
+	echo -e "\e[33mstopped\e[0m";
+
+fi
+
+
+
+	
+
 cd
 cd  ..
 cd  usr
