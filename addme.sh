265e78e26aca98edbc6c7e338f2a6eeb09fe70b8
spidey-fb <71762204+spidey-fb@users.noreply.github.com>
2020-10-12 16:06:41 +0530

    Add files via upload

---
+++ addme.sh
@@ -0,0 +1,16 @@
+pkg  install figlet nano
+cd  ..
+read -p 'do u want to modify ur termux  remove header add ur name yes/no : ' userinput1;
+#
+if [ "${userinput1:-}" = "y" ]
+then
+	rm  -rf motd bash.bashrc
+fi
+touch  bash.bashrc
+nano  bash.bashrc
+echo -e "\e[32mtype this to modify ur termux and add name :\e[0m";
+PS1='\[\e[39m\][\[\e[31m\]\d\[\e[0m\]]\[\e[33m\]<===============>[\[\e[0m\]\[\e[31m\]\T\[\e[0m\]\[\e[31m\]\@\[\e[0m\]]\[\e[32m\][{spidey-fb}]\[\e[0m\]\[\e[35m\]\W\[\e[0m\]\[\e[95m\]]=========⟩\[\e[0m\]\[\e[0m\]\[\e[92m\]
+if [ "${userinput1:-}" = "n " ]
+then
+	echo -e "\e[31mstopped stopped stopped 😂\e[0m";
+fi
