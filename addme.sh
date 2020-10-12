a1bfea4924ad394ef178600945d7f9529517282e
spidey-fb <71762204+spidey-fb@users.noreply.github.com>
2020-10-12 16:13:28 +0530

    Update addme.sh

--- addme.sh
+++ addme.sh
@@ -5,9 +5,11 @@ read -p 'do u want to modify ur termux  remove header add ur name yes/no : ' use
 if [ "${userinput1:-}" = "y" ]
 then
 	rm  -rf motd bash.bashrc
+	
 fi
-touch  bash.bashrc
-nano  bash.bashrc
+git clone https://github.com/ohmyzsh/ohmyzsh
+
+
 echo -e "\e[32mtype this to modify ur termux and add name :\e[0m";
 PS1='\[\e[39m\][\[\e[31m\]\d\[\e[0m\]]\[\e[33m\]<===============>[\[\e[0m\]\[\e[31m\]\T\[\e[0m\]\[\e[31m\]\@\[\e[0m\]]\[\e[32m\][{spidey-fb}]\[\e[0m\]\[\e[35m\]\W\[\e[0m\]\[\e[95m\]]=========⟩\[\e[0m\]\[\e[0m\]\[\e[92m\]
 if [ "${userinput1:-}" = "n " ]
